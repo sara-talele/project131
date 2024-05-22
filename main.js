2d9617d2c1dc1911528d600ec4024b06cd2a24be
sara-talele <125667061+sara-talele@users.noreply.github.com>
2024-05-22 20:24:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+img1 = "";
+img2 = "";
+img3 = "";
+
+function preload() {
+    img1 = loadImage("tree.jpg");
+    img2 = loadImage("Tour-Eiffel.jpg");
+    img3 = loadImage("images.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
